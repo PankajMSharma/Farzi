c18327339842286fa37c417d6b28ecd36431bcac
Pankaj Sharma <pankajmsharma357@hotmail.com>
2019-03-13 02:32:47 +0530

    Responsive Sidebar with main content

---
+++ src/app/layout/page-content/page-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DashboardComponent } from 'src/app/pages/dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-page-content',
+  templateUrl: './page-content.component.html',
+  styleUrls: ['./page-content.component.scss']
+})
+export class PageContentComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
